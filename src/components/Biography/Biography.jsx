c9bbf49e62be56cd7f1327156b5be76116b5f0f8
Iris M <morais.iris@gmail.com>
2025-01-27 00:05:33 +0000

    Added Biography component

---
+++ src/components/Biography/Biography.jsx
@@ -0,0 +1,13 @@
+import styles from "./Biography.module.scss"
+
+const Biography = () => {
+  return (
+    <div className={styles.biography}>
+      <div className={styles.biographyCopy}>
+        <p>After over 10 years in the food industry, I’m seeking a career change into tech. I’ve completed a bootcamp Software Engineering course to allow me to learn the latest methodologies in the field. Having strong management and organisational skills has allowed me to learn a new area and research skills have supported me on the learning journey and problem solving.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Biography;
